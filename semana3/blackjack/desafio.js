5cb9320285ac1272d7ece220d9ed0491b9537e72
Julio Gabriel <juliogabriel@outlook.com>
2020-06-05 16:56:23 -0300

    Exercício e dois desafio concluído

---
+++ semana3/blackjack/desafio.js
@@ -0,0 +1,87 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+console.log("Bem vindo ao jogo de Blackjack!")
+
+let sorteio = 0
+let cartasUsuario = []
+let cartasComputador = []
+let valorUsuario = []
+let valorComputador = []
+let pontuacaoUsuario = 0
+let pontuacaoComputador = 0
+let mensagem
+let trava = false
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+
+      for (sorteio = 0; sorteio <= 4; sorteio++) {
+         if (sorteio = 1) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+         }
+         if (sorteio = 2) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+         }
+         if (sorteio = 3) {
+            const carta = comprarCarta(); 
+            cartasComputador.push(carta.texto)
+            valorComputador.push(carta.valor)
+            pontuacaoComputador += carta.valor
+         }
+         if (sorteio = 4) {
+            const carta = comprarCarta(); 
+            cartasComputador.push(carta.texto)
+            valorComputador.push(carta.valor)
+            pontuacaoComputador += carta.valor
+         }
+         if ((valorUsuario[0] === valorUsuario[1]) || (valorComputador[0] === valorComputador[1])) {
+            sorteio = 0
+            pontuacaoUsuario = 0
+            pontuacaoComputador = 0
+            valorUsuario = []
+            valorComputador = []
+            cartasUsuario = []
+            cartasComputador = []  
+         }
+      }    
+         while ((pontuacaoUsuario < 21) && (trava === false)) {
+            if (pontuacaoUsuario > pontuacaoComputador) {
+               mensagem = ". O usuário ganhou!"
+            } else if (pontuacaoUsuario === pontuacaoComputador) {
+               mensagem = ". Empate!"
+            } else {
+               mensagem = ". O computador ganhou!"
+            }
+            if (confirm("Suas cartas são " + cartasUsuario + ". A carta revelada do computador é " + cartasComputador[0] + "\n" 
+            + "Deseja comprar mais uma carta?")) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+            } else {
+               trava = true;
+               alert("Suas cartas são "  + cartasUsuario + ". Sua pontuação é " + pontuacaoUsuario + ". As cartas do computador são " + cartasComputador + ". A pontuação do computador é " + pontuacaoComputador + mensagem)
+            }
+         }
+         if (pontuacaoUsuario => 21) {
+            alert("Suas cartas são "  + cartasUsuario + ". Sua pontuação é " + pontuacaoUsuario + ". As cartas do computador são " + cartasComputador + ". A pontuação do computador é " + pontuacaoComputador + mensagem)
+         }
+} else {
+   console.log("O jogo acabou")
+}
