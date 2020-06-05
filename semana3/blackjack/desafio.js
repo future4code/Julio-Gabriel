68e67b00d9d24d680a6a563b83bc5b76f998a4c0
Julio Gabriel <juliogabriel@outlook.com>
2020-06-05 19:34:30 -0300

    BlackJack concluído

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -11,6 +11,8 @@
  * 
  */
 
+/* Desafios: 8, 9 e 10 
+
 console.log("Bem vindo ao jogo de Blackjack!")
 
 let sorteio = 0
@@ -61,13 +63,6 @@ if(confirm("Quer iniciar uma nova rodada?")) {
          }
       }    
          while ((pontuacaoUsuario < 21) && (trava === false)) {
-            if (pontuacaoUsuario > pontuacaoComputador) {
-               mensagem = ". O usuário ganhou!"
-            } else if (pontuacaoUsuario === pontuacaoComputador) {
-               mensagem = ". Empate!"
-            } else {
-               mensagem = ". O computador ganhou!"
-            }
             if (confirm("Suas cartas são " + cartasUsuario + ". A carta revelada do computador é " + cartasComputador[0] + "\n" 
             + "Deseja comprar mais uma carta?")) {
             const carta = comprarCarta();
@@ -76,12 +71,182 @@ if(confirm("Quer iniciar uma nova rodada?")) {
             pontuacaoUsuario += carta.valor
             } else {
                trava = true;
-               alert("Suas cartas são "  + cartasUsuario + ". Sua pontuação é " + pontuacaoUsuario + ". As cartas do computador são " + cartasComputador + ". A pontuação do computador é " + pontuacaoComputador + mensagem)
             }
          }
-         if (pontuacaoUsuario => 21) {
-            alert("Suas cartas são "  + cartasUsuario + ". Sua pontuação é " + pontuacaoUsuario + ". As cartas do computador são " + cartasComputador + ". A pontuação do computador é " + pontuacaoComputador + mensagem)
+         if ((pontuacaoUsuario => 21) || (trava === true)) {
+            if (pontuacaoUsuario > pontuacaoComputador) {
+               mensagem = "O usuário ganhou!"
+            } else if (pontuacaoUsuario === pontuacaoComputador) {
+               mensagem = "Empate!"
+            } else {
+               mensagem = "O computador ganhou!"
+            }
+            alert("Suas cartas são "  + cartasUsuario + ". Sua pontuação é " + pontuacaoUsuario + "." + "\n" + "As cartas do computador são " + cartasComputador +  ". A pontuação do computador é " + pontuacaoComputador + "\n" + mensagem)
          }
+} else {
+   console.log("O jogo acabou")
+}
+
+*/
+
+/* Desafio 11
+
+console.log("Bem vindo ao jogo de Blackjack!")
+
+let sorteio = 0
+let cartasUsuario = []
+let cartasComputador = []
+let valorUsuario = []
+let valorComputador = []
+let pontuacaoUsuario = 0
+let pontuacaoComputador = 0
+let mensagem
+let trava = false
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+
+      for (sorteio = 0; sorteio <= 4; sorteio++) {
+         if (sorteio = 1) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+         }
+         if (sorteio = 2) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+         }
+         if (sorteio = 3) {
+            const carta = comprarCarta(); 
+            cartasComputador.push(carta.texto)
+            valorComputador.push(carta.valor)
+            pontuacaoComputador += carta.valor
+         }
+         if (sorteio = 4) {
+            const carta = comprarCarta(); 
+            cartasComputador.push(carta.texto)
+            valorComputador.push(carta.valor)
+            pontuacaoComputador += carta.valor
+         }
+         if ((valorUsuario[0] === valorUsuario[1]) || (valorComputador[0] === valorComputador[1])) {
+            sorteio = 0
+            pontuacaoUsuario = 0
+            pontuacaoComputador = 0
+            valorUsuario = []
+            valorComputador = []
+            cartasUsuario = []
+            cartasComputador = []  
+         }
+      }    
+         while ((pontuacaoUsuario <= 21) && (trava === false)) {
+            if (confirm("Suas cartas são " + cartasUsuario + ". A carta revelada do computador é " + cartasComputador[0] + "\n" 
+            + "Deseja comprar mais uma carta?")) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+            } else {
+               while ((pontuacaoUsuario <= 21) && (pontuacaoUsuario > pontuacaoComputador)) {
+                  const carta = comprarCarta();
+                  cartasComputador.push(carta.texto)
+                  valorComputador.push(carta.valor)
+                  pontuacaoComputador += carta.valor 
+               }
+               trava = true;
+            }
+         }
+         if (pontuacaoUsuario > pontuacaoComputador) {
+            mensagem = "O usuário ganhou!"
+         } else if (pontuacaoUsuario === pontuacaoComputador) {
+            mensagem = "Empate!"
+         } else {
+            mensagem = "O computador ganhou!"
+         }
+         alert("Suas cartas são "  + cartasUsuario + ". Sua pontuação é " + pontuacaoUsuario + "." + "\n" + "As cartas do computador são " + cartasComputador +  ". A pontuação do computador é " + pontuacaoComputador + "\n" + mensagem)
+} else {
+   console.log("O jogo acabou")
+}
+
+*/
+
+/* Desafio 12
+
+console.log("Bem vindo ao jogo de Blackjack!")
+
+let sorteio = 0
+let cartasUsuario = []
+let cartasComputador = []
+let valorUsuario = []
+let valorComputador = []
+let pontuacaoUsuario = 0
+let pontuacaoComputador = 0
+let mensagem
+let trava = false
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+
+      for (sorteio = 0; sorteio <= 4; sorteio++) {
+         if (sorteio = 1) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+         }
+         if (sorteio = 2) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+         }
+         if (sorteio = 3) {
+            const carta = comprarCarta(); 
+            cartasComputador.push(carta.texto)
+            valorComputador.push(carta.valor)
+            pontuacaoComputador += carta.valor
+         }
+         if (sorteio = 4) {
+            const carta = comprarCarta(); 
+            cartasComputador.push(carta.texto)
+            valorComputador.push(carta.valor)
+            pontuacaoComputador += carta.valor
+         }
+         if ((valorUsuario[0] === valorUsuario[1]) || (valorComputador[0] === valorComputador[1])) {
+            sorteio = 0
+            pontuacaoUsuario = 0
+            pontuacaoComputador = 0
+            valorUsuario = []
+            valorComputador = []
+            cartasUsuario = []
+            cartasComputador = []  
+         }
+      }    
+         while ((pontuacaoUsuario <= 21) && (trava === false)) {
+            if (confirm("Suas cartas são " + cartasUsuario + ". A carta revelada do computador é " + cartasComputador[0] + "\n" 
+            + "Deseja comprar mais uma carta?")) {
+            const carta = comprarCarta();
+            cartasUsuario.push(carta.texto)
+            valorUsuario.push(carta.valor)
+            pontuacaoUsuario += carta.valor
+            } else {
+               while((pontuacaoUsuario <= 21) && (pontuacaoUsuario > pontuacaoComputador)) {
+                  const carta = comprarCarta();
+                  cartasComputador.push(carta.texto)
+                  valorComputador.push(carta.valor)
+                  pontuacaoComputador += carta.valor 
+               }
+               trava = true;
+            }
+         }
+         if (((pontuacaoUsuario > pontuacaoComputador) && (pontuacaoUsuario <= 21)) || ((pontuacaoComputador > 21) && (pontuacaoUsuario < 21))) {
+            mensagem = "O usuário ganhou!"
+         } else if (pontuacaoUsuario === pontuacaoComputador) {
+            mensagem = "Empate!"
+         } else {
+            mensagem = "O computador ganhou!"
+         }
+         alert("Suas cartas são "  + cartasUsuario + ". Sua pontuação é " + pontuacaoUsuario + "." + "\n" + "As cartas do computador são " + cartasComputador +  ". A pontuação do computador é " + pontuacaoComputador + "\n" + mensagem)
 } else {
    console.log("O jogo acabou")
 }
