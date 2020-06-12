8b1475f23ae48dfc58d7a88b4a35d4deeab47c5d
Julio Gabriel <juliogabriel@outlook.com>
2020-06-12 11:11:38 -0300

    Exercício 2 de interpretação de código

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,9 +1,25 @@
-/* Exercício 1
+/* Exercício 1 - Interpretação de código
 
 Uma função chamada conversorDeMoeda recebe um parâmetro chamadado valorEmDolar, e guarda outro valor que corresponde 
 ao valor atual da cotação do dolar que vai receber do usúario através do prompt numa variável chamada cotacao e a 
 função retorna a mensagem R$ e o resultado da multiplicação de valorEmDolar e cotacao, depois ele atribui o valor de
 uma variável a função conversorDeMoeda com o paramêtro 100 e imprime na tela o valor da multiplicação de 100 e o valor
 que o usuário digitar.
+
+*/
+
+/* Exercicio 2 - Interpretação de código
+
+Uma função chamada investeDinheiro recebe dois paramêtros que são tipoDeInvestimentoe e valor. O switch fica verificando 
+se o tipoDeInvestimento é igual as strings Poupança Renda Fixa, CDB, Ações e caso não são seja nenhum
+deles vai imprimir uma alerta no navegador informando que o tipo de investimento é incorreto, caso seja uma das strings 
+ele atribui o valor da multiplicação do parametro valor e de uma constante que depende de cada tipo de investimento para 
+uma variável chamada valorAposInvestimento, por exemplo, se for poupança ele multiplica o parametro valor recebido pela 
+função por 1.03 e para de verificar, caso seja Renda fixa multiplica o valor por 1.05, caso seja CDB multiplica o valor 
+por 1.06 e caso seja Ações ele multiplica por 1.1. A função retorna a variavel valorAposInvestimento. Atribui e chama
+a função investeDinheiro com os parametros "Ações" e 150 a variavel novoMontante e também atribui e chama a função
+investeDinheiro com os parametros "TesouroDireto, 200" a varivel segundoMontante e depois imprime o resultado no console
+de cada uma delas. O primeiro resultado obtido será 165, já o segundo resulto, como não existe a string Tesouto Direto
+imprimirá a mensagem TIPO DE INVESTIMENTO INFORMADO INCORRETO! 
 
 */
