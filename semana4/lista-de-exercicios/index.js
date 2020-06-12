a6cecc7104bd33fa8898869f81d32edbc01d7844
Julio Gabriel <juliogabriel@outlook.com>
2020-06-12 10:48:04 -0300

    Exercício 1 de interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,9 @@
+/* Exercício 1
+
+Uma função chamada conversorDeMoeda recebe um parâmetro chamadado valorEmDolar, e guarda outro valor que corresponde 
+ao valor atual da cotação do dolar que vai receber do usúario através do prompt numa variável chamada cotacao e a 
+função retorna a mensagem R$ e o resultado da multiplicação de valorEmDolar e cotacao, depois ele atribui o valor de
+uma variável a função conversorDeMoeda com o paramêtro 100 e imprime na tela o valor da multiplicação de 100 e o valor
+que o usuário digitar.
+
+*/
