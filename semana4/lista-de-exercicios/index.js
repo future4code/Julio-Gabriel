9863647c4d3ede4ac23c8076194eb1150183157c
Julio Gabriel <juliogabriel@outlook.com>
2020-06-13 07:56:02 -0300

    Exercício 3 - Exercícios de Funções de array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -313,6 +313,35 @@ console.log(dobraArray, triplicaArray, verificaPar)
 
 */
 
+/* Exercício 3 - Exercícios de Funções de array 
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const pessoasAutorizadas = pessoas.filter((pessoa, index, array) => {
+    if (((pessoa.idade > 14) && (pessoa.idade < 60)) && (pessoa.altura >= 1.5)) {
+        return true
+    }
+})
+
+const pessoasNaoAutorizadas = pessoas.filter((pessoa, index, array) => {
+    if (((pessoa.idade <= 14) || (pessoa.idade >= 60)) || (pessoa.altura < 1.5)) {
+        return true
+    }
+})
+
+console.log(pessoasAutorizadas)
+console.log(pessoasNaoAutorizadas)
+
+*/
+
+
 
 
 
