0b27ec9ccc0da7e7dbe22a6a58a5f66ad89a5a87
Julio Gabriel <juliogabriel@outlook.com>
2020-07-14 11:03:10 -0300

    Renderização do perfil da pessoa na página Home

--- semana9/astromatch/src/components/Home.js
+++ semana9/astromatch/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 
@@ -9,14 +9,39 @@ height: 80vh;
 display: flex;
 flex-direction: column;
 align-items: center;
+justify-content: space-around;
 `
 
 const ContainerBotoes = styled.div
 `
+width: 28vw;
 display: flex;
-justify-content: center;
+justify-content: space-around;
+border: 1px solid black;
 `
 
+const InfoPerfil = styled.div
+`
+width: 30vw;
+height: 60vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+border: 1px solid blue;
+`
+
+const NomeIdade = styled.div
+`
+position: absolute;
+width: 28vw;
+`
+
+const Biografia = styled.p
+`
+position: absolute;
+width: 28vw;
+`
 const BotaoNaoGostei = styled.button
 `
 border: 1px solid red;
@@ -42,17 +67,29 @@ background: transparent;
 border-radius: 100%;
 `
 
+const ImagemPerfil = styled.img
+`
+position: absolute;
+width: 28vw;
+height: 60vh;
+border-radius: 5px;
+box-shadow: 3px 3px 3px 3px rgba(50, 50, 50, 50);
+`
+
 function Home() {
 
     const [perfilExibido, setPerfilExibido] = useState([])
 
     const aluno = "julio-gabriel-turing"
 
+    useEffect(() => {
+        retornaPerfil(aluno)
+    }, [])
+
     const retornaPerfil = (aluno) => {
         axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${aluno}/person`,)
         .then((response) => {
             setPerfilExibido(response.data.profile)
-            console.log(perfilExibido)
         })
         .catch((error) => {
             console.log(error.message)
@@ -61,7 +98,11 @@ function Home() {
 
     return (
         <ContainerHome>
-            <p>Home</p>
+            <InfoPerfil>
+                <ImagemPerfil src={perfilExibido.photo}></ImagemPerfil>
+                <NomeIdade>{perfilExibido.name}, {perfilExibido.age}</NomeIdade>
+                <Biografia>{perfilExibido.bio}</Biografia>
+            </InfoPerfil>
             <ContainerBotoes>
                 <BotaoNaoGostei>X</BotaoNaoGostei>
                 <BotaoGostei>❤</BotaoGostei>
