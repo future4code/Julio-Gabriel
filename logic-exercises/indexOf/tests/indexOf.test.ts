0f33beccbe835aabccc77f3783b1e44b669629ba
Julio Gabriel <juliogabriel@outlook.com>
2020-11-04 12:38:17 -0300

    Exercício de lógica IndexOf

---
+++ logic-exercises/indexOf/tests/indexOf.test.ts
@@ -0,0 +1,21 @@
+import { indexOf } from '../src/index'
+
+describe("Testa a função para procurar o index do caractere que você deseja na string", () => {
+   test("Deve retornar 0 e 3", async () => {
+      expect.assertions(2)
+
+      const result: number = indexOf("hoje", "h")
+      const resultTwo: number = indexOf("hoje", "e")
+      
+      expect(result).toEqual(0)
+      expect(resultTwo).toEqual(3)
+   })
+
+   test("Deve retornar -1, pois não encontrou o caractere na string", async () => {
+      expect.assertions(1)
+
+      const result: number = indexOf("hoje", "k")
+      
+      expect(result).toEqual(-1)
+   }) 
+})
